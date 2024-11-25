c25d8aac377b7369fab2339292a0cc2f8fac8572
Julian <julian.mjm.300@gmail.com>
2024-11-24 22:36:51 -0800

    adding compare function for constraints

--- include/cbs.h
+++ include/cbs.h
@@ -30,6 +30,15 @@ struct Constraint {
     uint timeStep;
 
     bool isGoalWall;
+
+    bool operator==(const Constraint& other) const {
+        bool idEqual = this->agentId == other.agentId;
+        bool timeEqual = this->timeStep == other.timeStep;
+        bool typeEqual = this->location.isEdgeCollision == other.location.isEdgeCollision;
+        bool l1Equal = this->location.l1 == other.location.l1;
+        bool l2Equal = this->location.l2 == other.location.l2;
+        return (idEqual && timeEqual && typeEqual && l1Equal && l2Equal);
+    }
 };
 
 struct AStarNode {
