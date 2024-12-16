20f0693d0468b9f1f8c4c6e87a6ddbde23b4ccac
sunilm33 <78884617+sunilm33@users.noreply.github.com>
2024-12-16 14:05:52 -0800

    something i guess

--- include/mdd.h
+++ include/mdd.h
@@ -17,15 +17,48 @@
 #include "dg.h"
 #include "wdg.h"
 
-struct MDDNode{
+struct MDDNode {
     int id; //Unique ID
     int timestep;
+    AStarLocation nodeLoc;
     std::vector<MDDNode*> parents; // Cases where 2 or more nodes can be a parent of a node
     std::vector<MDDNode*> children;
 
-    MDDNode(int nodeID, int timestepID) : id(nodeID), timestep(timestepID) {}
-
+    MDDNode(MDDNode *parent, AStarLocation loc) : nodeLoc(loc) {
+        if(parent == nullptr) // no parent = root
+        {
+            timestep = 0;
+        }
+        else
+        {
+            timestep = parent->timestep + 1;
+            parents.push_back(parent);
+        }
+    };
+    bool operator==(const MDDNode &other) const{
+        if(this->nodeLoc == other.nodeLoc && this->timestep == other.timestep){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 };
-struct MDD{
+struct MDD {
+    // Must maintain size of cost * vertices?
+    std::vector<std::vector<MDDNode*>> timesteps;
+    int maxTimestep;
+    int totalNodes;
+    // Every agent has a start and goal
+    int startNode;
+    int goalNode;
+
+    MDD(int maxTimeStep);
+
+    // Add to specific timestep, add an edge to the parent and return nodeID
+    int addNode(int timeStep, int parentID);
+
+    // Find a specific node without know its timestep?
+    MDDNode findNode(int nodeID);
 
 };
