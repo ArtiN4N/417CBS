6382c6221a2c16793171f12f888b56af1a6d9fb1
artinan <artinan30@protonmail.com>
2024-12-17 12:23:50 -0800

    added dg test load

---
+++ src/dg/dg_test_load.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include "../../include/map.h"
+#include "../../include/defs.h"
+#include "../../include/cbs.h"
+
+int main(int argc, char* argv[]) {
+    HeuristicType type = DG;
+
+    Map map = {};
+    map.initAgents();
+    map.loadFromFile("instances/test_50.txt");
+
+    std::vector<AStarPath> soln = findSolution(map, type, "useless.txt");
+    if (soln.size() > 0) std::cout << "fount solution with " << map.nAgents << " agents\n";
+
+    map.destroy();
+    return 0;
+}
