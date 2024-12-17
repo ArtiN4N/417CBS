9d078f77e3f7404951fef0ef01a12af69ea70407
artinan <artinan30@protonmail.com>
2024-12-17 14:12:33 -0800

    testing cg and dg

--- src/cg/cg_test_load.cpp
+++ src/cg/cg_test_load.cpp
@@ -7,13 +7,17 @@
 int main(int argc, char* argv[]) {
     HeuristicType type = CG;
 
-    Map map = {};
-    map.initAgents();
-    map.loadFromFile("instances/test_50.txt");
+    for (int i = 1; i <= 50; i++) {
+        Map map = {};
+        map.initAgents();
+        map.loadFromFile("instances/test_" + std::to_string(i) + ".txt");
 
-    std::vector<AStarPath> soln = findSolution(map, type, "useless.txt");
-    if (soln.size() > 0) std::cout << "fount solution with " << map.nAgents << " agents\n";
+        std::vector<AStarPath> soln = findSolution(map, type, "useless.txt");
+        if (soln.size() > 0) std::cout << "fount solution with " << map.nAgents << " agents for map << " << i << "\n";
 
-    map.destroy();
+        map.destroy();
+    }
+
+    
     return 0;
 }
